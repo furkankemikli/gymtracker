4cd8c341498cf00d8534d6c57d8fa0558e6098ae
Furkan Kemikli <furkan.kemikli@tosiatech.com>
2018-03-05 14:43:38 +0300

    Calendar, logo and some page recommemdations added

--- trAssist/Controllers/HomeController.cs
+++ trAssist/Controllers/HomeController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using trAssist.Models;
 
 namespace trAssist.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-        
+
+        public IActionResult NewTrainee()
+        {
+            return View();
+        }
+
 
         public IActionResult About()
         {
